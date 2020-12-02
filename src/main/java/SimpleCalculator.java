d6a3718008c4621c26c5f0d0668bc75581521f69
hernerd <55166743+hernerd@users.noreply.github.com>
2020-12-02 11:31:06 -0500

    new functions

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -11,6 +11,18 @@ public class SimpleCalculator {
     public int div(int a, int b) {
         return a / b;
     }
+    public int square(int a) {
+        return a * a;
+    }
+    public int fact(int a) {
+        int fact = 1;
+        int i = 1;
+        while( i <= a){
+            fact = fact * i;
+            i++;
+        }
+        return fact;
+    }
     public static final void main(String[] args) {
         System.out.println("Hey there CIS350.");
     }
